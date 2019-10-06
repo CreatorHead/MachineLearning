f5932403df3bdead935ae37e4d1356457a384b99
creatorHead <droidbit1@gmail.com>
2019-10-07 00:10:07 +0530

    small changes

--- kNN.py
+++ kNN.py
@@ -6,7 +6,7 @@ import kNN
 Python also has the ability to return multiple values from a function call ,
 something missing from many other languages. In this case the return values should
 be a comma-separated list of values and Python then constructs a tuple and returns
-this to the caller
+this to the  caller
 """
 def createDataSet():
     group = array([[1.0, 1.1],
