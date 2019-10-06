8c906b912f8208b698d82bf47f952c8124701190
creatorHead <droidbit1@gmail.com>
2019-10-06 23:45:07 +0530

    Notes on Working with Forms, TextBoxes, Checkboxes and Radio Buttons

--- kNN.py
+++ kNN.py
@@ -3,7 +3,7 @@ import operator
 import kNN
 
 """
-Python also has the ability to return multiple values from a function call,
+Python also has the ability to return multiple values from a function call ,
 something missing from many other languages. In this case the return values should
 be a comma-separated list of values and Python then constructs a tuple and returns
 this to the caller
