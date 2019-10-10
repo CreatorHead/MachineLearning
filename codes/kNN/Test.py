7128fcdcbbbcc23d6d26c69480ddd410e4c095ed
creatorHead <droidbit1@gmail.com>
2019-10-10 12:22:52 +0530

    handwriting Class Test added

--- codes/kNN/Test.py
+++ codes/kNN/Test.py
@@ -1,12 +1,12 @@
 from codes.kNN.create_scatter_plots_with_Matplotlib import *
-from codes.kNN.kNN import autoNorm, datingClassTest, classifyPerson
+from codes.kNN.kNN import autoNorm, datingClassTest, classifyPerson, img2Vector, handwritingClassTest
 
 """ First Test Sample """
 # group, labels = create_dataSet()
 # print(classify0([0, 0], group, labels, 3))
 
 """ 2nd Test Sample: Dating ML Model """
-#draw_time_spent_in_video_game_VS_Ice_cream_consumed()
+# draw_time_spent_in_video_game_VS_Ice_cream_consumed()
 # draw_Flyier_Mies_VS_time_spent_in_video_game()
 
 
@@ -15,6 +15,12 @@ from codes.kNN.kNN import autoNorm, datingClassTest, classifyPerson
 # normMat, ranges, minVals = autoNorm(datingDataMat)
 # print(normMat)
 
-#datingClassTest()
+# datingClassTest()
 
-classifyPerson()
+# classifyPerson()
+
+# filePath = pathlib.Path(__file__).parent / '../../resources/testDigits/0_13.txt'
+# testVector = img2Vector(filePath)
+# print(testVector[0, 0:31])
+
+handwritingClassTest()
--- codes/kNN/kNN.py
+++ codes/kNN/kNN.py
@@ -1,4 +1,5 @@
 import pathlib
+from os import listdir
 
 from numpy import *
 import operator
@@ -89,5 +90,49 @@ def classifyPerson():
     normMat, ranges, minVals = autoNorm(datingDataMat)
     inArr = array([ffMiles, percentTats, iceCream])
     classifierResult = classify0((inArr - minVals) / ranges, normMat, datingLabels, 3)
-    print("You will probably like this person: " , resultList[classifierResult - 1])
+    print("You will probably like this person: ", resultList[classifierResult - 1])
 
+
+def img2Vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+
+def img2vector(param):
+    pass
+
+
+def handwritingClassTest():
+    hwLabels = []
+    filePath = pathlib.Path(__file__).parent / '../../resources/testDigits'
+    testVector = img2Vector(filePath)
+    trainingFileList = listdir('trainingDigits')  # load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        filePath = pathlib.Path(__file__).parent / '../../resources/testDigits/%s'
+        trainingMat[i, :] = img2vector(filePath % fileNameStr)
+    filePath = pathlib.Path(__file__).parent / '../../resources/testDigits'
+    testFileList = listdir(filePath)  # iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        filePath = pathlib.Path(__file__).parent / '../../resources/testDigits/%s'
+        vectorUnderTest = img2vector(filePath % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount / float(mTest)))
