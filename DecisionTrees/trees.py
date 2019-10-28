ac70d1b477ac2f9f665903f3be419ce0186a8281
creatorHead <droidbit1@gmail.com>
2019-10-29 01:13:44 +0530

    tree algo added

--- DecisionTrees/trees.py
+++ DecisionTrees/trees.py
@@ -1,4 +1,30 @@
-from DecisionTrees.calcShanonEntropy import calcShannonEnt
+from math import log
+
+'''
+The code is straightforward, first you calculate a count of the number of instances in the dataset.
+This could have been calculated inline, but it's used multiple times in the code, so an explicit 
+variable is created for it. Next, you create a dictionary whose keys are the values in the final 
+column. 
+1.  If a key was not encountered previously, one is created. For each key, you keep track of how many
+    times this label occurs. Finally, you use the frequency of all the different labels to calculate
+    the probability of that label. This probability is used to calculate the Shanon entropy.
+2.  And, you sum this up for all the labels.
+'''
+
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:  # the the number of unique elements and their occurance
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)  # log base 2
+    return shannonEnt
 
 
 def createDataSet():
@@ -11,6 +37,37 @@ def createDataSet():
     return dataSet, labels
 
 
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]  # chop out axis used for splitting
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1  # the last column is used for the labels
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0;
+    bestFeature = -1
+    for i in range(numFeatures):  # iterate over all the features
+        featList = [example[i] for example in dataSet]  # create a list of all the examples of this feature
+        uniqueVals = set(featList)  # get a set of unique values
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy  # calculate the info gain; ie reduction in entropy
+        if (infoGain > bestInfoGain):  # compare this to the best gain so far
+            bestInfoGain = infoGain  # if better than current best, set to best
+            bestFeature = i
+    return bestFeature  # returns an integer
+
+
 myDataSet, labels = createDataSet()
-entropy = calcShannonEnt(myDataSet)
-print(entropy)
+print(myDataSet)
+print(splitDataSet(myDataSet, 0, 1))
+print(splitDataSet(myDataSet, 0, 0))
