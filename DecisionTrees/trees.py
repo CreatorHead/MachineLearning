88011bd9ecd4b625855a86a5beeaece59334be61
creatorHead <droidbit1@gmail.com>
2019-10-31 13:13:05 +0530

    create Tree method added

--- DecisionTrees/trees.py
+++ DecisionTrees/trees.py
@@ -1,4 +1,5 @@
 from math import log
+import operator
 
 '''
 The code is straightforward, first you calculate a count of the number of instances in the dataset.
@@ -65,6 +66,33 @@ def chooseBestFeatureToSplit(dataSet):
             bestInfoGain = infoGain  # if better than current best, set to best
             bestFeature = i
     return bestFeature  # returns an integer
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]  # stop splitting when all of the classes are equal
+    if len(dataSet[0]) == 1:  # stop splitting when there are no more features in dataSet
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del (labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]  # copy all of labels, so trees don't mess up existing labels
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
 
 
 myDataSet, labels = createDataSet()
